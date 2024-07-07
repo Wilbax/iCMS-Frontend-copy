003d2eb4edd8a7376b86d3fc34b087d0623ae1d8
Hirusha Fernando <heshanhfernando@gmail.com>
2024-07-08 01:01:08 +0530

    url

--- src/app/shared/helpers/token.interceptor.ts
+++ src/app/shared/helpers/token.interceptor.ts
@@ -2,12 +2,13 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from "@angular/core";
 import { AuthenticationService } from "../../auth/services/authentication.service";
 import { switchMap } from "rxjs/operators";
+import { apiEndpoint } from "../../app-settings/config";
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthenticationService);
   const URLS_TO_EXCLUDE = [
     "api.ipify.org",
-    "3.7.55.235:8000"
+    apiEndpoint
   ]
 
   for (const url of URLS_TO_EXCLUDE) {
