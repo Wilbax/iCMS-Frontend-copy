4812fc42c86c6a170efe6054e3f4b8863b29cb39
Wilbax <janithravisankax@gmail.com>
2024-07-08 10:49:39 +0530

    backend IPs changed

--- src/app/app-settings/config.ts
+++ src/app/app-settings/config.ts
@@ -2,7 +2,7 @@
 
 // export const apiEndpoint = 'http://localhost:8000';
 
-export const apiEndpoint = 'http://65.0.110.120:8000';
+export const apiEndpoint = 'http://13.201.176.236:8000';
 
 export const socialMediaBackendAPI = 'https://icsms-social-media-backend.vercel.app'
 //  export const socialMediaBackendAPI = 'http://localhost:8000'
